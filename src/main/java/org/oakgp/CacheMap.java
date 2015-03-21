13022be575bf39b70c3a5c039c40697dea4246cf
webber-s <webbers.github@gmail.com>
2015-03-21 10:15:32 +0000

    Added CacheMap

---
+++ src/main/java/org/oakgp/CacheMap.java
@@ -0,0 +1,24 @@
+package org.oakgp;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public final class CacheMap<K, V> extends LinkedHashMap<K, V> {
+	public static <K, V> Map<K, V> create(int maxSize) {
+		CacheMap<K, V> m = new CacheMap<>(maxSize);
+		return Collections.synchronizedMap(m);
+	}
+
+	private final int maxSize;
+
+	private CacheMap(int maxSize) {
+		super(maxSize, 0.1f, true);
+		this.maxSize = maxSize;
+	}
+
+	@Override
+	protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+		return size() > maxSize;
+	}
+}
---
+++ src/test/java/org/oakgp/CacheMapTest.java
@@ -0,0 +1,95 @@
+package org.oakgp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+public class CacheMapTest {
+	@Test
+	public void testSynchronized() {
+		assertEquals("java.util.Collections$SynchronizedMap", CacheMap.create(3).getClass().getName());
+	}
+
+	@Test
+	public void testSizeThree() {
+		final int maxSize = 3;
+		Map<String, Integer> m = CacheMap.create(maxSize);
+
+		m.put("a", 1);
+		m.put("b", 1);
+		m.put("c", 1);
+		m.put("d", 1);
+		m.put("e", 1);
+
+		assertEquals(maxSize, m.size());
+		assertTrue(m.containsKey("c"));
+		assertTrue(m.containsKey("d"));
+		assertTrue(m.containsKey("e"));
+
+		m.put("a", 1);
+
+		assertEquals(maxSize, m.size());
+		assertTrue(m.containsKey("a"));
+		assertTrue(m.containsKey("d"));
+		assertTrue(m.containsKey("e"));
+
+		m.get("d");
+		m.put("x", 1);
+		m.get("d");
+		m.put("y", 1);
+		m.get("d");
+		m.put("z", 1);
+
+		assertEquals(maxSize, m.size());
+		assertTrue(m.containsKey("d"));
+		assertTrue(m.containsKey("y"));
+		assertTrue(m.containsKey("z"));
+
+		m.get("y");
+		m.get("z");
+		m.put("a", 1);
+		m.get("y");
+		m.get("z");
+		m.put("b", 1);
+		m.get("y");
+		m.get("z");
+		m.put("x", 1);
+
+		assertEquals(maxSize, m.size());
+		assertTrue(m.containsKey("x"));
+		assertTrue(m.containsKey("y"));
+		assertTrue(m.containsKey("z"));
+
+		m.put("q", 1);
+		m.put("r", 1);
+		m.put("y", 1);
+
+		assertEquals(maxSize, m.size());
+		assertTrue(m.containsKey("q"));
+		assertTrue(m.containsKey("r"));
+		assertTrue(m.containsKey("y"));
+	}
+
+	@Test
+	public void testSizeTen() {
+		final int maxSize = 10;
+		Map<Integer, String> m = CacheMap.create(maxSize);
+
+		for (int i = 0; i <= maxSize; i++) {
+			m.put(i, "");
+		}
+		assertEquals(maxSize, m.size());
+		assertFalse(m.containsKey(0));
+
+		for (int i = 0; i <= maxSize; i++) {
+			m.put(i, "");
+			m.get(0);
+		}
+		assertEquals(maxSize, m.size());
+		assertTrue(m.containsKey(0));
+	}
+}
