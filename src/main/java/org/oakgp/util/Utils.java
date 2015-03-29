1fbdc89e4c4cbcc9f42eab69fb6fec5c1909165a
webber-s <webbers.github@gmail.com>
2015-03-29 11:12:21 +0100

    ArithmeticOperatorTest

--- src/main/java/org/oakgp/util/Utils.java
+++ src/main/java/org/oakgp/util/Utils.java
@@ -5,8 +5,16 @@ import org.oakgp.node.Node;
 import org.oakgp.serialize.NodeWriter;
 
 public final class Utils {
+	private static final Object[][] TEST_DATA = { { 2, 14, 4, 9, 7 } };
+
 	public static void assertEvaluateToSameResult(Node input, Node output) {
-		Assignments assignments = Assignments.createAssignments(0, 1, 2, 3, 1);
+		for (Object[] assignedValues : TEST_DATA) {
+			assertEvaluateToSameResult(input, output, assignedValues);
+		}
+	}
+
+	private static void assertEvaluateToSameResult(Node input, Node output, Object[] assignedValues) {
+		Assignments assignments = Assignments.createAssignments(assignedValues);
 		if (!input.evaluate(assignments).equals(output.evaluate(assignments))) {
 			throw new IllegalArgumentException(new NodeWriter().writeNode(input) + " = " + input.evaluate(assignments) + " "
 					+ new NodeWriter().writeNode(output) + " = " + output.evaluate(assignments));
--- src/test/java/org/oakgp/operator/ArithmeticOperatorTest.java
+++ src/test/java/org/oakgp/operator/ArithmeticOperatorTest.java
@@ -2,12 +2,36 @@ package org.oakgp.operator;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
+import static org.oakgp.TestUtils.createTypeArray;
+import static org.oakgp.TestUtils.readNode;
 import static org.oakgp.Type.INTEGER;
+import static org.oakgp.examples.SystemTestUtils.ARITHMETIC_FUNCTION_SET;
+import static org.oakgp.examples.SystemTestUtils.RANDOM;
+import static org.oakgp.examples.SystemTestUtils.RATIO_VARIABLES;
+import static org.oakgp.examples.SystemTestUtils.createConstants;
+import static org.oakgp.examples.SystemTestUtils.makeRandomTree;
+import static org.oakgp.util.Utils.assertEvaluateToSameResult;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
+import org.oakgp.NodeSimplifier;
 import org.oakgp.Signature;
+import org.oakgp.TerminalSet;
+import org.oakgp.node.ConstantNode;
+import org.oakgp.node.Node;
+import org.oakgp.serialize.NodeWriter;
 
 public class ArithmeticOperatorTest {
+	private static final File TEST_DATA_FILE = new File("src/test/resources/ArithmeticOperatorTest.txt");
+	private static final int TEST_DATA_SIZE = 10000;
+
 	@Test
 	public void testGetSignature() {
 		Operator o = new ArithmeticOperator() {
@@ -21,5 +45,40 @@ public class ArithmeticOperatorTest {
 		assertEquals(2, signature.getArgumentTypesLength());
 		assertSame(INTEGER, signature.getArgumentType(0));
 		assertSame(INTEGER, signature.getArgumentType(1));
+	}
+
+	@Test
+	public void testSimplification() throws IOException {
+		List<String> tests = getTestData();
+		assertEquals(TEST_DATA_SIZE, tests.size());
+		for (String test : tests) {
+			Node input = readNode(test);
+			Node output = new NodeSimplifier().simplify(input);
+			assertEvaluateToSameResult(input, output);
+		}
+	}
+
+	private List<String> getTestData() throws IOException {
+		if (!TEST_DATA_FILE.exists()) {
+			createTestData(TEST_DATA_FILE, TEST_DATA_SIZE);
+		}
+		return Files.readAllLines(TEST_DATA_FILE.toPath());
+	}
+
+	private static void createTestData(File outputFile, int size) throws IOException {
+		ConstantNode[] constants = createConstants(11);
+		int numVariables = 4;
+		TerminalSet terminalSet = new TerminalSet(RANDOM, RATIO_VARIABLES, createTypeArray(numVariables), constants);
+		Set<String> examples = new HashSet<>();
+		try (FileWriter fw = new FileWriter(outputFile)) {
+			while (examples.size() < size) {
+				Node node = makeRandomTree(ARITHMETIC_FUNCTION_SET, terminalSet, 16);
+				String example = new NodeWriter().writeNode(node);
+				if (node.getNodeCount() > 7 && examples.add(example)) {
+					fw.write(example);
+					fw.write('\n');
+				}
+			}
+		}
 	}
 }
