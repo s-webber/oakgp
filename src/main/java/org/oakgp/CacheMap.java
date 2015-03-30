18acb6f2fda26e798dccda944a83ad6b4f1c0dec
webber-s <webbers.github@gmail.com>
2015-03-30 13:53:38 +0100

    javadoc

--- src/main/java/org/oakgp/CacheMap.java
+++ src/main/java/org/oakgp/CacheMap.java
@@ -4,14 +4,27 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * Provides a size-limited map of keys to values.
+ * <p>
+ * When the maximum size limit has been reached, the least-recently accessed entry will be removed whenever a new entry is added.
+ */
 public final class CacheMap<K, V> extends LinkedHashMap<K, V> {
+	private final int maxSize;
+
+	/**
+	 * Returns a size-limited map of keys to values.
+	 *
+	 * @param maxSize
+	 *            the maximum size restriction to enforce on the returned map
+	 * @return a size-limited map of keys to values
+	 */
 	public static <K, V> Map<K, V> createCache(int maxSize) {
 		CacheMap<K, V> m = new CacheMap<>(maxSize);
 		return Collections.synchronizedMap(m);
 	}
 
-	private final int maxSize;
-
+	/** @see {@link #createCache(int)} */
 	private CacheMap(int maxSize) {
 		super(maxSize, 0.1f, true);
 		this.maxSize = maxSize;
