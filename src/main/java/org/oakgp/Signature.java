38135a9ab36ac3bd99dddcce56f129c955a5af2e
webber-s <webbers.github@gmail.com>
2015-04-03 09:44:22 +0100

    added toString/equals/hashCode to Signature

--- src/main/java/org/oakgp/Signature.java
+++ src/main/java/org/oakgp/Signature.java
@@ -1,8 +1,11 @@
 package org.oakgp;
 
+import java.util.Arrays;
+
 public final class Signature { // TODO move to org.oakgp.operator?
    private final Type returnType;
    private final Type[] argumentTypes;
+   private int hashCode;
 
    public static Signature createSignature(Type returnType, Type... argumentTypes) {
       Type[] copy = new Type[argumentTypes.length];
@@ -13,6 +16,7 @@ public final class Signature { // TODO move to org.oakgp.operator?
    private Signature(Type returnType, Type[] argumentTypes) {
       this.returnType = returnType;
       this.argumentTypes = argumentTypes;
+      this.hashCode = (returnType.hashCode() * 31) * Arrays.hashCode(argumentTypes);
    }
 
    public Type getReturnType() {
@@ -25,5 +29,25 @@ public final class Signature { // TODO move to org.oakgp.operator?
 
    public int getArgumentTypesLength() { // TODO rename?
       return argumentTypes.length;
+   }
+
+   @Override
+   public int hashCode() {
+      return hashCode;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (o instanceof Signature) {
+         Signature s = (Signature) o;
+         return this.returnType == s.returnType && Arrays.equals(this.argumentTypes, s.argumentTypes);
+      } else {
+         return false;
+      }
+   }
+
+   @Override
+   public String toString() {
+      return returnType + " " + Arrays.toString(argumentTypes);
    }
 }
--- src/test/java/org/oakgp/SignatureTest.java
+++ src/test/java/org/oakgp/SignatureTest.java
@@ -1,7 +1,9 @@
 package org.oakgp;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.oakgp.Signature.createSignature;
 import static org.oakgp.Type.BOOLEAN;
@@ -31,5 +33,60 @@ public class SignatureTest {
       } catch (ArrayIndexOutOfBoundsException e) {
          // expected
       }
+   }
+
+   @Test
+   public void testToString() {
+      Signature signature = createSignature(INTEGER, BOOLEAN, INTEGER, BOOLEAN);
+      assertEquals("INTEGER [BOOLEAN, INTEGER, BOOLEAN]", signature.toString());
+   }
+
+   @Test
+   public void testEquals() {
+      Signature s1 = createSignature(INTEGER, BOOLEAN, INTEGER, BOOLEAN);
+      Signature s2 = createSignature(INTEGER, BOOLEAN, INTEGER, BOOLEAN);
+      assertEquals(s1.hashCode(), s2.hashCode());
+      assertTrue(s1.equals(s1));
+      assertTrue(s1.equals(s2));
+      assertTrue(s2.equals(s1));
+   }
+
+   @Test
+   public void testNotEquals() {
+      Signature s1 = createSignature(INTEGER, BOOLEAN, INTEGER, BOOLEAN);
+      Signature s2 = createSignature(INTEGER, INTEGER, BOOLEAN, BOOLEAN);
+      Signature s3 = createSignature(BOOLEAN, BOOLEAN, INTEGER, BOOLEAN);
+      Signature s4 = createSignature(INTEGER, BOOLEAN, INTEGER, BOOLEAN, INTEGER);
+      Signature s5 = createSignature(INTEGER, BOOLEAN, INTEGER, BOOLEAN, BOOLEAN);
+
+      assertTrue(s1.equals(s1));
+      assertFalse(s1.equals(s2));
+      assertFalse(s1.equals(s3));
+      assertFalse(s1.equals(s4));
+      assertFalse(s1.equals(s5));
+
+      assertTrue(s2.equals(s2));
+      assertFalse(s2.equals(s1));
+      assertFalse(s2.equals(s3));
+      assertFalse(s2.equals(s4));
+      assertFalse(s2.equals(s5));
+
+      assertTrue(s3.equals(s3));
+      assertFalse(s3.equals(s1));
+      assertFalse(s3.equals(s2));
+      assertFalse(s3.equals(s4));
+      assertFalse(s3.equals(s5));
+
+      assertTrue(s4.equals(s4));
+      assertFalse(s4.equals(s1));
+      assertFalse(s4.equals(s2));
+      assertFalse(s4.equals(s3));
+      assertFalse(s4.equals(s5));
+
+      assertTrue(s5.equals(s5));
+      assertFalse(s5.equals(s1));
+      assertFalse(s5.equals(s2));
+      assertFalse(s5.equals(s3));
+      assertFalse(s5.equals(s4));
    }
 }
