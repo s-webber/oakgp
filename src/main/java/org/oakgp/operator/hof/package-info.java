8791fb0ea64f29e94a7049a6384354f14e2902d8
webber-s <webbers.github@gmail.com>
2015-04-03 09:54:28 +0100

    package-info.java

---
+++ src/main/java/org/oakgp/operator/hof/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides higher-order functions.
+ * <p>
+ * A higher-order function is a function that does at least one of the following:
+ * <ul><li>Takes one or more functions as input.</li><li>Returns a function.</li></ul>
+ *
+ * @see <a href="http://en.wikipedia.org/wiki/Higher-order_function">Wikipedia</a>
+ */
+package org.oakgp.operator.hof;
+
