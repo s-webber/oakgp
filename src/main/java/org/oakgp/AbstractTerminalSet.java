552ca5f8bf48aca641d2566fe243fd6ae8a66a06
webber-s <webbers.github@gmail.com>
2015-04-05 10:27:38 +0100

    added Utils.groupNodesByType

--- src/main/java/org/oakgp/AbstractTerminalSet.java
+++ src/main/java/org/oakgp/AbstractTerminalSet.java
@@ -1,30 +1,16 @@
 package org.oakgp;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.oakgp.node.Node;
+import org.oakgp.util.Utils;
 
 abstract class AbstractTerminalSet<T extends Node> {
    private final Map<Type, List<T>> nodesByType;
 
    AbstractTerminalSet(T[] nodes) {
-      nodesByType = new HashMap<>();
-      for (T n : nodes) {
-         put(nodesByType, n.getType(), n);
-      }
-   }
-
-   // TODO generify this logic and reuse elsewhere
-   private void put(Map<Type, List<T>> map, Type key, T value) {
-      List<T> list = map.get(key);
-      if (list == null) {
-         list = new ArrayList<>();
-         map.put(key, list);
-      }
-      list.add(value);
+      nodesByType = Utils.groupNodesByType(nodes);
    }
 
    // TODO return immutable version
--- src/main/java/org/oakgp/util/Utils.java
+++ src/main/java/org/oakgp/util/Utils.java
@@ -1,9 +1,34 @@
 package org.oakgp.util;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.oakgp.Type;
+import org.oakgp.node.Node;
+
 /** Utility methods that support the functionality provided by the rest of the framework. */
 public final class Utils {
    /** Private constructor as all methods are static. */
    private Utils() {
       // do nothing
+   }
+
+   public static <T extends Node> Map<Type, List<T>> groupNodesByType(T[] nodes) {
+      Map<Type, List<T>> nodesByType = new HashMap<>();
+      for (T n : nodes) {
+         addToListOfMap(nodesByType, n.getType(), n);
+      }
+      return nodesByType;
+   }
+
+   public static <T> void addToListOfMap(Map<Type, List<T>> map, Type key, T value) {
+      List<T> list = map.get(key);
+      if (list == null) {
+         list = new ArrayList<>();
+         map.put(key, list);
+      }
+      list.add(value);
    }
 }
