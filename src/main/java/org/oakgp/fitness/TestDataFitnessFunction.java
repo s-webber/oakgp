ea381f087ed04b0102aa99c93da666e85237a2f7
webber-s <webbers.github@gmail.com>
2015-04-11 19:14:13 +0100

    added rankingFunction to TestDataFitnessFunction
    The result of node.evaluate(input) might not always be an int - so now
    allow a function to be specified in constructor which converts the
    result of evaluate to an int.

--- src/main/java/org/oakgp/fitness/TestDataFitnessFunction.java
+++ src/main/java/org/oakgp/fitness/TestDataFitnessFunction.java
@@ -1,6 +1,7 @@
 package org.oakgp.fitness;
 
 import java.util.Map;
+import java.util.function.ToIntFunction;
 
 import org.oakgp.Assignments;
 import org.oakgp.node.Node;
@@ -8,6 +9,7 @@ import org.oakgp.node.Node;
 /** Calculates the fitness of a potential solution by comparing its results against the expected values. */
 public final class TestDataFitnessFunction implements FitnessFunction {
    private final Map<Assignments, Integer> tests;
+   private final ToIntFunction<Object> rankingFunction;
 
    /**
     * Constructs a new {@code FitnessFunction} which uses the specified test data to assess the fitness of potential solutions.
@@ -16,7 +18,12 @@ public final class TestDataFitnessFunction implements FitnessFunction {
     *           test data which associates a collection of inputs with their expected outcomes
     */
    public TestDataFitnessFunction(Map<Assignments, Integer> tests) {
+      this(tests, (o) -> (int) o);
+   }
+
+   public TestDataFitnessFunction(Map<Assignments, Integer> tests, ToIntFunction<Object> rankingFunction) {
       this.tests = tests;
+      this.rankingFunction = rankingFunction;
    }
 
    /**
@@ -32,9 +39,7 @@ public final class TestDataFitnessFunction implements FitnessFunction {
       for (Map.Entry<Assignments, Integer> test : tests.entrySet()) {
          Assignments input = test.getKey();
          int expected = test.getValue();
-         // TODO in future, the result of node.evaluate(input) might not always be an int
-         // TODO allow a function to be specified in constructor which converts the result of evaluate to an int
-         int actual = (int) node.evaluate(input);
+         int actual = rankingFunction.applyAsInt(node.evaluate(input));
          diff += Math.abs(actual - expected);
       }
       return diff;
--- src/test/java/org/oakgp/fitness/TestDataFitnessFunctionTest.java
+++ src/test/java/org/oakgp/fitness/TestDataFitnessFunctionTest.java
@@ -14,7 +14,7 @@ import org.oakgp.node.Node;
 
 public class TestDataFitnessFunctionTest {
    @Test
-   public void test() {
+   public void testDefaultRankingFunction() {
       // test data
       Map<Assignments, Integer> testData = new HashMap<>();
       Assignments assignments1 = createAssignments(1);
@@ -36,5 +36,30 @@ public class TestDataFitnessFunctionTest {
 
       // assert result
       assertEquals(8d, result, 0d);
+   }
+
+   @Test
+   public void testSpecifiedRankingFunction() {
+      // test data
+      Map<Assignments, Integer> testData = new HashMap<>();
+      Assignments assignments1 = createAssignments(1);
+      testData.put(assignments1, 9);
+      Assignments assignments2 = createAssignments(2);
+      testData.put(assignments2, 2);
+      Assignments assignments3 = createAssignments(3);
+      testData.put(assignments3, 7);
+
+      // mock
+      Node mockNode = mock(Node.class);
+      given(mockNode.evaluate(assignments1)).willReturn("abcdefg");
+      given(mockNode.evaluate(assignments2)).willReturn("asdf");
+      given(mockNode.evaluate(assignments3)).willReturn("qwerty");
+
+      // invoke evaluate method
+      FitnessFunction fitnessFunction = new TestDataFitnessFunction(testData, (o) -> ((String) o).length());
+      double result = fitnessFunction.evaluate(mockNode);
+
+      // assert result
+      assertEquals(5d, result, 0d);
    }
 }
