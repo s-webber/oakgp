e3a8e2796c324aedefde43943150bfddaf6f4540
webber-s <webbers.github@gmail.com>
2015-04-18 19:06:39 +0100

    exception messages

--- src/test/java/org/oakgp/util/DummyValuesMap.java
+++ src/test/java/org/oakgp/util/DummyValuesMap.java
@@ -18,9 +18,9 @@ public class DummyValuesMap<K, V> {
    public V next(K key) {
       DummyValuesQueue<V> values = map.get(key);
       if (values == null) {
-         throw new IllegalArgumentException("No entry found for key: " + key);
+         throw new IllegalArgumentException("No entry found for key: " + key + " of type: " + key.getClass().getName());
       } else if (values.isEmpty()) {
-         throw new IllegalArgumentException("No remaning values found for key: " + key + " of type: " + key.getClass().getName());
+         throw new IllegalArgumentException("No remaining values found for key: " + key + " of type: " + key.getClass().getName());
       } else {
          return values.next();
       }
@@ -35,7 +35,7 @@ public class DummyValuesMap<K, V> {
 
       public Builder<K, V> put(K key, DummyValuesQueue<V> values) {
          if (map.containsKey(key)) {
-            throw new IllegalStateException("Vakues already exist for key: " + key);
+            throw new IllegalStateException("Values already exist for key: " + key);
          } else {
             map.put(key, values);
          }
