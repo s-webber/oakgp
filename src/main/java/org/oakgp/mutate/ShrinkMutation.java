fc0ff71a23b4ea957b6e250f3d827115a5739504
webber-s <webbers.github@gmail.com>
2015-04-25 09:24:07 +0100

    ShrinkMutation added

---
+++ src/main/java/org/oakgp/mutate/ShrinkMutation.java
@@ -0,0 +1,40 @@
+package org.oakgp.mutate;
+
+import java.util.function.Predicate;
+
+import org.oakgp.NodeEvolver;
+import org.oakgp.PrimitiveSet;
+import org.oakgp.node.FunctionNode;
+import org.oakgp.node.Node;
+import org.oakgp.selector.NodeSelector;
+import org.oakgp.util.Random;
+
+public class ShrinkMutation implements NodeEvolver {
+   private final Random random;
+   private final PrimitiveSet primitiveSet;
+
+   public ShrinkMutation(Random random, PrimitiveSet primitiveSet) {
+      this.random = random;
+      this.primitiveSet = primitiveSet;
+   }
+
+   @Override
+   public Node evolve(NodeSelector selector) {
+      Node root = selector.next();
+      Predicate<Node> treeWalkerStrategy = n -> n instanceof FunctionNode;
+      int nodeCount = root.getNodeCount(treeWalkerStrategy);
+      if (nodeCount == 0) {
+         // if nodeCount == 0 then that indicates that 'root' is a terminal node
+         // (so can't be shrunk any further)
+         return root;
+      } else if (nodeCount == 1) {
+         // if node count == 1 then that indicates that 'root' is a function node
+         // that only has terminal nodes as arguments
+         return primitiveSet.nextAlternativeTerminal(root);
+      } else {
+         // Note: -1 to avoid selecting root node
+         int index = random.nextInt(nodeCount - 1);
+         return root.replaceAt(index, (n) -> primitiveSet.nextAlternativeTerminal(n), treeWalkerStrategy);
+      }
+   }
+}
---
+++ src/test/java/org/oakgp/mutate/ShrinkMutationTest.java
@@ -0,0 +1,71 @@
+package org.oakgp.mutate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.oakgp.TestUtils.integerConstant;
+import static org.oakgp.TestUtils.readNode;
+import static org.oakgp.TestUtils.writeNode;
+import static org.oakgp.util.DummyRandom.GetIntExpectation.nextInt;
+
+import org.junit.Test;
+import org.oakgp.PrimitiveSet;
+import org.oakgp.node.ConstantNode;
+import org.oakgp.node.Node;
+import org.oakgp.selector.DummyNodeSelector;
+import org.oakgp.util.DummyPrimitiveSet;
+import org.oakgp.util.DummyRandom;
+import org.oakgp.util.Random;
+
+public class ShrinkMutationTest {
+   @Test
+   public void testTerminal() {
+      ConstantNode constant = integerConstant(1);
+      Node result = shrinkMutate(DummyRandom.EMPTY, new DummyPrimitiveSet(), constant);
+      assertSame(constant, result);
+   }
+
+   @Test
+   public void testFunctionNodeWithTerminalArguments() {
+      Node input = readNode("(+ v0 v1)");
+      ConstantNode expectedResult = integerConstant(42);
+      PrimitiveSet primitiveSet = new DummyPrimitiveSet() {
+         @Override
+         public Node nextAlternativeTerminal(Node nodeToReplace) {
+            assertSame(input, nodeToReplace);
+            return expectedResult;
+         }
+      };
+      Node result = shrinkMutate(DummyRandom.EMPTY, primitiveSet, input);
+      assertSame(expectedResult, result);
+   }
+
+   @Test
+   public void testFunctionNode() {
+      Node input = readNode("(+ (+ (if (zero? v0) 7 8) v1) (+ 9 v2))");
+      DummyNodeSelector selector = DummyNodeSelector.repeat(4, input);
+      ConstantNode expectedResult = integerConstant(42);
+      PrimitiveSet primitiveSet = new DummyPrimitiveSet() {
+         @Override
+         public Node nextAlternativeTerminal(Node nodeToReplace) {
+            return expectedResult;
+         }
+      };
+      DummyRandom random = nextInt(4).returns(1, 3, 0, 2);
+      ShrinkMutation mutator = new ShrinkMutation(random, primitiveSet);
+
+      assertEquals("(+ (+ 42 v1) (+ 9 v2))", writeNode(mutator.evolve(selector)));
+      assertEquals("(+ (+ (if (zero? v0) 7 8) v1) 42)", writeNode(mutator.evolve(selector)));
+      assertEquals("(+ (+ (if 42 7 8) v1) (+ 9 v2))", writeNode(mutator.evolve(selector)));
+      assertEquals("(+ 42 (+ 9 v2))", writeNode(mutator.evolve(selector)));
+
+      selector.assertEmpty();
+      random.assertEmpty();
+   }
+
+   private Node shrinkMutate(Random random, PrimitiveSet primitiveSet, Node input) {
+      DummyNodeSelector selector = new DummyNodeSelector(input);
+      Node result = new ShrinkMutation(random, primitiveSet).evolve(selector);
+      selector.assertEmpty();
+      return result;
+   }
+}
