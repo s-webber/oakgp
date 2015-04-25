9f161148607156e961f2f335bbd3fd46748943de
webber-s <webbers.github@gmail.com>
2015-04-25 09:35:13 +0100

    SubTreeMutation added

---
+++ src/main/java/org/oakgp/mutate/SubTreeMutation.java
@@ -0,0 +1,31 @@
+package org.oakgp.mutate;
+
+import org.oakgp.NodeEvolver;
+import org.oakgp.Type;
+import org.oakgp.node.Node;
+import org.oakgp.selector.NodeSelector;
+import org.oakgp.util.Random;
+import org.oakgp.util.Utils;
+
+public class SubTreeMutation implements NodeEvolver {
+   private final Random random;
+   private final TreeGenerator treeGenerator;
+
+   public SubTreeMutation(Random random, TreeGenerator treeGenerator) {
+      this.random = random;
+      this.treeGenerator = treeGenerator;
+   }
+
+   @Override
+   public Node evolve(NodeSelector selector) {
+      Node root = selector.next();
+      int mutationPoint = Utils.selectSubNodeIndex(root, random);
+      return root.replaceAt(mutationPoint, node -> {
+         return treeGenerator.generate(node.getType(), node.getDepth());
+      });
+   }
+
+   public static interface TreeGenerator {
+      Node generate(Type type, int depth);
+   }
+}
---
+++ src/test/java/org/oakgp/mutate/SubTreeMutationTest.java
@@ -0,0 +1,23 @@
+package org.oakgp.mutate;
+
+import static org.junit.Assert.assertEquals;
+import static org.oakgp.TestUtils.integerConstant;
+import static org.oakgp.TestUtils.readNode;
+import static org.oakgp.TestUtils.writeNode;
+import static org.oakgp.util.DummyRandom.GetIntExpectation.nextInt;
+
+import org.junit.Test;
+import org.oakgp.node.ConstantNode;
+import org.oakgp.node.Node;
+import org.oakgp.selector.DummyNodeSelector;
+
+public class SubTreeMutationTest {
+   @Test
+   public void test() {
+      Node input = readNode("(+ (+ (if (zero? v0) 7 8) v1) (+ 9 v2))");
+      DummyNodeSelector selector = new DummyNodeSelector(input);
+      ConstantNode result = integerConstant(42);
+      SubTreeMutation mutator = new SubTreeMutation(nextInt(10).returns(5), (t, d) -> result);
+      assertEquals("(+ (+ (if (zero? v0) 7 8) 42) (+ 9 v2))", writeNode(mutator.evolve(selector)));
+   }
+}
