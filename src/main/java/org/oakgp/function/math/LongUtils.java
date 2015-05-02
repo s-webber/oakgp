a4946e86801cbf4c1cddec39ad38b195dc85e892
webber-s <webbers.github@gmail.com>
2015-05-02 10:52:20 +0100

    LongUtils added

---
+++ src/main/java/org/oakgp/function/math/LongUtils.java
@@ -0,0 +1,115 @@
+package org.oakgp.function.math;
+
+import org.oakgp.Assignments;
+import org.oakgp.Type;
+import org.oakgp.node.ConstantNode;
+import org.oakgp.node.Node;
+
+public class LongUtils implements NumberUtils {
+   private static final Type TYPE = Type.longType();
+
+   public static final LongUtils LONG_UTILS = new LongUtils();
+
+   private final ArithmeticExpressionSimplifier simplifier = new ArithmeticExpressionSimplifier(this);
+   private final ConstantNode zero = createConstant(0);
+   private final ConstantNode one = createConstant(1);
+   private final ConstantNode two = createConstant(2);
+   private final Add add = new Add(this);
+   private final Subtract subtract = new Subtract(this);
+   private final Multiply multiply = new Multiply(this);
+
+   /** @see #INTEGER_UTILS */
+   private LongUtils() {
+      // do nothing
+   }
+
+   @Override
+   public Type getType() {
+      return TYPE;
+   }
+
+   @Override
+   public ArithmeticExpressionSimplifier getSimplifier() {
+      return simplifier;
+   }
+
+   @Override
+   public Add getAdd() {
+      return add;
+   }
+
+   @Override
+   public Subtract getSubtract() {
+      return subtract;
+   }
+
+   @Override
+   public Multiply getMultiply() {
+      return multiply;
+   }
+
+   @Override
+   public ConstantNode negate(Node n) {
+      return createConstant(-(long) n.evaluate(null));
+   }
+
+   @Override
+   public boolean isZero(Node n) {
+      return zero.equals(n);
+   }
+
+   @Override
+   public boolean isOne(Node n) {
+      return one.equals(n);
+   }
+
+   @Override
+   public ConstantNode zero() {
+      return zero;
+   }
+
+   @Override
+   public ConstantNode one() {
+      return one;
+   }
+
+   @Override
+   public ConstantNode two() {
+      return two;
+   }
+
+   @Override
+   public boolean isNegative(Node n) {
+      return ((int) n.evaluate(null)) < 0;
+   }
+
+   @Override
+   public ConstantNode add(Node n1, Node n2, Assignments assignments) {
+      long i1 = n1.evaluate(assignments);
+      long i2 = n2.evaluate(assignments);
+      return createConstant(i1 + i2);
+   }
+
+   @Override
+   public ConstantNode add(Node n, int i) {
+      return createConstant((long) n.evaluate(null) + i);
+   }
+
+   @Override
+   public ConstantNode subtract(Node n1, Node n2, Assignments assignments) {
+      long i1 = n1.evaluate(assignments);
+      long i2 = n2.evaluate(assignments);
+      return createConstant(i1 - i2);
+   }
+
+   @Override
+   public ConstantNode multiply(Node n1, Node n2, Assignments assignments) {
+      long i1 = n1.evaluate(assignments);
+      long i2 = n2.evaluate(assignments);
+      return createConstant(i1 * i2);
+   }
+
+   private ConstantNode createConstant(long i) {
+      return new ConstantNode(i, TYPE);
+   }
+}
