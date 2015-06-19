0ce1d96259686010c06bc453f39fef8c0b240366
s-webber <webbers.github@gmail.com>
2015-06-19 20:17:51 +0100

    added package-info.java to org.oakgp.examples.hanoi

--- src/test/java/org/oakgp/examples/hanoi/TowersOfHanoi.java
+++ src/test/java/org/oakgp/examples/hanoi/TowersOfHanoi.java
@@ -2,19 +2,7 @@ package org.oakgp.examples.hanoi;
 
 import java.util.Arrays;
 
-/**
- * Towers of Hanoi
- * <p>
- * The Towers of Hanoi is a game played with three poles and a number of discs of different sizes which can slide onto any pole. The game starts with all the
- * discs stacked in ascending order of size on one pole, the smallest at the top. The aim of the game is to move the entire stack to another pole, obeying the
- * following rules:
- * <ul>
- * <li>Only one disc may be moved at a time.</li>
- * <li>Each move consists of taking the upper disc from one of the poles and sliding it onto another pole.</li>
- * <li>No disc may be placed on top of a smaller one.</li>
- * </ul>
- * </p>
- */
+/** A model of the Towers of Hanoi problem. */
 class TowersOfHanoi {
    private static final int NUM_POLES = Pole.values().length;
 
