b45b3f674b627cd1e180a38baebdc03965948c78
s-webber <webbers.github@gmail.com>
2015-06-19 20:19:03 +0100

    added package-info to org.oakgp.examples.gridwar

--- src/test/java/org/oakgp/examples/gridwar/GridWar.java
+++ src/test/java/org/oakgp/examples/gridwar/GridWar.java
@@ -5,18 +5,7 @@ import org.oakgp.node.Node;
 import org.oakgp.tournament.TwoPlayerGame;
 import org.oakgp.util.Random;
 
-/**
- * Game engine for Grid War.
- * <p>
- * Grid War is a two-player turn-based zero-sum board game. It is played on a 4x4 grid. Players take turns to move. Players can move one square at a time in one
- * of four directions (up, down, left or right). If a player selects a move that would cause them to exit the board then they forfeit their turn. A player wins
- * if they move to a square that is already occupied by their opponent. A player loses if they move in the same direction on two consecutive moves.
- * </p>
- * <p>
- * The game is described in the O'Reilly book "Programming Collective Intelligence" by Tony Segaran (ISBN 978-0-596-52932-1). <a
- * href="https://www.safaribooksonline.com/library/view/programming-collective-intelligence/9780596529321/ch11s08.html">View extract.</a>
- * </p>
- */
+/** Game engine for Grid War. */
 class GridWar implements TwoPlayerGame {
    static final int GRID_WIDTH = 4;
    private static final int NUMBER_OF_POSSIBLE_DIRECTIONS = 4;
---
+++ src/test/java/org/oakgp/examples/gridwar/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Grid War
+ * <p>
+ * Grid War is a two-player turn-based zero-sum board game. It is played on a 4x4 grid. Players take turns to move. Players can move one square at a time in one
+ * of four directions (up, down, left or right). If a player selects a move that would cause them to exit the board then they forfeit their turn. A player wins
+ * if they move to a square that is already occupied by their opponent. A player loses if they move in the same direction on two consecutive moves.
+ * </p>
+ * <p>
+ * The game is described in the O'Reilly book "Programming Collective Intelligence" by Tony Segaran (ISBN 978-0-596-52932-1). <a
+ * href="https://www.safaribooksonline.com/library/view/programming-collective-intelligence/9780596529321/ch11s08.html">View extract.</a>
+ * </p>
+ */
+package org.oakgp.examples.gridwar;
