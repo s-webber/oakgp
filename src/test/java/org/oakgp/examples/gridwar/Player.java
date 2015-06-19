63339a086458398fa0dcd737e1bafdad5a7e8516
s-webber <webbers.github@gmail.com>
2015-06-19 20:19:14 +0100

    reformatting

--- src/test/java/org/oakgp/examples/gridwar/Player.java
+++ src/test/java/org/oakgp/examples/gridwar/Player.java
@@ -21,20 +21,20 @@ class Player {
    void updateState(int nextMove) {
       previousMove = nextMove;
       switch (nextMove) {
-      case 0:
-         up();
-         break;
-      case 1:
-         right();
-         break;
-      case 2:
-         down();
-         break;
-      case 3:
-         left();
-         break;
-      default:
-         throw new IllegalArgumentException("Invalid move: " + nextMove);
+         case 0:
+            up();
+            break;
+         case 1:
+            right();
+            break;
+         case 2:
+            down();
+            break;
+         case 3:
+            left();
+            break;
+         default:
+            throw new IllegalArgumentException("Invalid move: " + nextMove);
       }
    }
 
