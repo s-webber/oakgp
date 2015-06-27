2f279cdfca923a0d182b85efab37d917c924337b
s-webber <webbers.github@gmail.com>
2015-06-27 09:47:02 +0100

    MaxGenerationTerminatorTest added

---
+++ src/test/java/org/oakgp/terminate/MaxGenerationTerminatorTest.java
@@ -0,0 +1,24 @@
+package org.oakgp.terminate;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+import org.oakgp.RankedCandidate;
+import org.oakgp.node.Node;
+
+public class MaxGenerationTerminatorTest {
+   @Test
+   public void test() {
+      MaxGenerationTerminator t = new MaxGenerationTerminator(3);
+      List<RankedCandidate> candidates = Collections.singletonList(new RankedCandidate(mock(Node.class), 1));
+      assertFalse(t.test(candidates));
+      assertFalse(t.test(candidates));
+      assertFalse(t.test(candidates));
+      assertTrue(t.test(candidates));
+   }
+}
