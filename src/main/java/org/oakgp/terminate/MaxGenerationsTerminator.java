574ce71f2b1e8bcbc14a817b87257def0c102e4c
s-webber <webbers.github@gmail.com>
2015-06-27 10:22:15 +0100

    no longer keep record of previous best candidate

--- src/main/java/org/oakgp/terminate/MaxGenerationsTerminator.java
+++ src/main/java/org/oakgp/terminate/MaxGenerationsTerminator.java
@@ -9,7 +9,6 @@ import org.oakgp.RankedCandidate;
 public final class MaxGenerationsTerminator implements Predicate<List<RankedCandidate>> {
    private final int maxGenerations;
    private int ctr = 0;
-   private double previousBest = 0;
 
    public MaxGenerationsTerminator(int maxGenerations) {
       this.maxGenerations = maxGenerations;
@@ -18,14 +17,9 @@ public final class MaxGenerationsTerminator implements Predicate<List<RankedCand
    @Override
    public boolean test(List<RankedCandidate> t) {
       ctr++;
-      RankedCandidate bestCandidate = t.get(0);
-      double bestRank = bestCandidate.getFitness();
-      boolean finished = ctr > maxGenerations || bestRank == 0;
-      if (previousBest != bestRank) {
-         previousBest = bestRank;
-         Logger.getGlobal().info(ctr + " " + bestRank + " " + bestCandidate);
-      } else if (finished || ctr % 100 == 0) {
-         Logger.getGlobal().info(ctr + " " + bestRank);
+      boolean finished = ctr > maxGenerations;
+      if (finished || ctr % 100 == 0) {
+         Logger.getGlobal().info(ctr + " " + t.get(0).getFitness());
       }
       return finished;
    }
