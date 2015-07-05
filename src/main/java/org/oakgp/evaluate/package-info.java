72f2e549193c0bb53746e79ec8f69678c2c1e961
s-webber <webbers.github@gmail.com>
2015-07-05 14:00:33 +0100

    added package-info.java

---
+++ src/main/java/org/oakgp/evaluate/package-info.java
@@ -0,0 +1,2 @@
+/** Provides classes for the evaluation and ranking of candidates in a generation. */
+package org.oakgp.evaluate;
---
+++ src/main/java/org/oakgp/primitive/package-info.java
@@ -0,0 +1,2 @@
+/** Provides classes for specifying the contents of a primitive set. */
+package org.oakgp.primitive;
